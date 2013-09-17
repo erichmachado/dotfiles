b59f9532a31d06c73e78c90985a60136f2862d46
Erich Soares Machado <erichmachado@gmail.com>
2013-09-17 19:06:22 -0300

    Added Bash shopt settings

---
+++ bash/config/options.sh
@@ -0,0 +1,8 @@
+# Append to the history file, don't overwrite it
+shopt -s histappend
+ 
+# Store multi-line commands in the history as a single command
+shopt -s cmdhist
+ 
+# Check the window size after each command and, if necessary, update the values of LINES and COLUMNS
+shopt -s checkwinsize
