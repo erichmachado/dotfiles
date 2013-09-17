1d116327ee62711675e86e389795e3b6e734ae6d
Erich Soares Machado <erichmachado@gmail.com>
2013-09-17 19:11:40 -0300

    History settings for Bash

---
+++ bash/config/history.sh
@@ -0,0 +1,11 @@
+# Don't put duplicate lines and lines beginning with a space character in the history. See bash(1) for more options
+HISTCONTROL=ignoreboth
+ 
+# Don't record in history the simple invocation of 'ls' without any arguments, and the shell built-ins bg, fg, and exit
+HISTIGNORE="ls:[bf]g:exit"
+ 
+# Setting history entries length in memory
+HISTSIZE=100000
+ 
+# Setting history entries length in file
+HISTFILESIZE=100000
