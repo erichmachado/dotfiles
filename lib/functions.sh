d0df8352c297c36ac7fd5d4a42fc758214590c83
Erich Soares Machado <erichmachado@gmail.com>
2013-09-26 01:02:23 -0300

    Added _include() helper function

--- lib/functions.sh
+++ lib/functions.sh
@@ -1,4 +1,17 @@
 # Helper functions
+
+# Includes the provided source files in the current script.
+# Automatically references files relative to $FRESH_LOCAL.
+# Paths with globbing must be provided around double quotes to process all files.
+# Example:
+# _include "lib/*.sh"
+_include()
+{
+  for script in "$FRESH_LOCAL"/$1; do
+    [[ -f $script ]] && source $script
+  done
+}
+
 _installed()
 {
   test $(which $1)
