eff9a0c400232e7ec4525532fb331fa7beaa1f35
Erich Soares Machado <erichmachado@gmail.com>
2015-10-11 11:22:05 +0200

    Set bottom left corner to put the display to sleep instead of running the screensaver

--- osx/defaults/screensaver.sh
+++ osx/defaults/screensaver.sh
@@ -4,6 +4,6 @@ defaults write com.apple.screensaver askForPassword -int 1
 # Set to immediately require password after sleep or screen saver begins
 defaults write com.apple.screensaver askForPasswordDelay -float 0
 
-# Run the screensaver if we're in the bottom-left hot corner.
-defaults write com.apple.dock wvous-bl-corner -int 5
+# Put display to sleep if we're in the bottom-left hot corner.
+defaults write com.apple.dock wvous-bl-corner -int 10
 defaults write com.apple.dock wvous-bl-modifier -int 0
