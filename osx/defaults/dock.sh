a8dbe66455caa81881c9f83e405c050a0440717f
Erich Soares Machado <erichmachado@gmail.com>
2015-10-11 11:49:36 +0200

    Toggled effect to show which applications are hidden on Dock

--- osx/defaults/dock.sh
+++ osx/defaults/dock.sh
@@ -2,4 +2,8 @@
 defaults write com.apple.dock wvous-bl-corner -int 10
 defaults write com.apple.dock wvous-bl-modifier -int 0
 
+# Toggle effect to show which applications are hidden
+defaults write com.apple.dock showhidden -bool true
+
+# Restarting Dock so that changes can take effect
 killall Dock
