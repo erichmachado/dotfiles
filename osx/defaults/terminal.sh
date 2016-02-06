ce9bed756a3891adcfe81d3c3f5399b65ade20d0
Erich Soares Machado <erichmachado@gmail.com>
2016-02-06 10:17:08 +0100

    Updated OS X Terminal settings

--- osx/defaults/terminal.sh
+++ osx/defaults/terminal.sh
@@ -1,8 +1,10 @@
+terminal_profile=Pro
+
 # Set default Terminal profile to Homebrew
-defaults write com.apple.Terminal "Default Window Settings" Homebrew
+defaults write com.apple.Terminal "Default Window Settings" $terminal_profile
 # Set startup Terminal profile to Homebrew
-defaults write com.apple.Terminal "Startup Window Settings" Homebrew
+defaults write com.apple.Terminal "Startup Window Settings" $terminal_profile
 # Set Homebrew profile columns to 160
-plutil -replace "Window Settings".Homebrew.columnCount -integer 160 $HOME/Library/Preferences/com.apple.Terminal.plist
+plutil -replace "Window Settings".$terminal_profile.columnCount -integer 160 $HOME/Library/Preferences/com.apple.Terminal.plist
 # Set Homebrew profile rows to 48
-plutil -replace "Window Settings".Homebrew.rowCount -integer 48 $HOME/Library/Preferences/com.apple.Terminal.plist
+plutil -replace "Window Settings".$terminal_profile.rowCount -integer 48 $HOME/Library/Preferences/com.apple.Terminal.plist
