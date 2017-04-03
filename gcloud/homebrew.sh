491844d0728691dcf2cc0332699427947d59b58e
Erich Soares Machado <erichmachado@gmail.com>
2017-04-03 15:26:32 +0200

    Add Google Cloud SDK setup on Homebrew

---
+++ gcloud/homebrew.sh
@@ -0,0 +1,3 @@
+# Google Cloud SDK setup
+source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc'
+source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc'
